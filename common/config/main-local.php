e17a36307718065c6367e232b26af341b380f11d
Maksim Balukh <maxb@iMac-Maksim.local>
2015-09-01 01:52:57 +0300

    Test fix

--- common/config/main-local.php
+++ common/config/main-local.php
@@ -2,21 +2,42 @@
 //Имя пользователя: webadmin
 //Пароль: sbncotJnfY
 
-return [
-    'components' => [
-        'db' => [
-            'class' => 'yii\db\Connection',
-            'dsn' => 'pgsql:host=ec2-54-235-151-252.compute-1.amazonaws.com;dbname=d3m1jkh1saukl9',
-            'username' => 'nvwwtvmauftsvy',
-            'password' => 'T4Tl019smi9ETrJQHHc8GxxzPj',
-            'charset' => 'utf8',
+if($_SERVER['HTTP_HOST']=="miu-portal.lizasoft.com"){
+
+    return [
+        'components' => [
+            'db' => [
+                'class' => 'yii\db\Connection',
+                'dsn' => 'pgsql:host=127.0.0.1;dbname=photobook',
+                'username' => 'postgres',
+                'password' => 'Max31960',
+                'charset' => 'utf8',
+            ],
+            'mailer' => [
+                'class' => 'yii\swiftmailer\Mailer',
+                'viewPath' => '@common/mail',
+            ],
         ],
-        'mailer' => [
-            'class' => 'yii\swiftmailer\Mailer',
-            'viewPath' => '@common/mail',
+    ];
+
+
+}else{
+    return [
+        'components' => [
+            'db' => [
+                'class' => 'yii\db\Connection',
+                'dsn' => 'pgsql:host=ec2-54-235-151-252.compute-1.amazonaws.com;dbname=d3m1jkh1saukl9',
+                'username' => 'nvwwtvmauftsvy',
+                'password' => 'T4Tl019smi9ETrJQHHc8GxxzPj',
+                'charset' => 'utf8',
+            ],
+            'mailer' => [
+                'class' => 'yii\swiftmailer\Mailer',
+                'viewPath' => '@common/mail',
+            ],
         ],
-    ],
-];
+    ];
+}
 /*
 return [
     'components' => [
