9ea640df7d882309c6f246cc8fbbead793b6ddf3
Maksim Balukh <maxb@iMac-Maksim.local>
2015-09-02 20:20:47 +0300

    fixes

--- frontend/controllers/BaseController.php
+++ frontend/controllers/BaseController.php
@@ -29,7 +29,7 @@ class BaseController extends Controller
 
         //$css_file_id='';
 
-        $test=9;
+        $test=10;
 
         $ref = Yii::$app->request->get('ref', '');
         $id = Yii::$app->request->get('id', '');
